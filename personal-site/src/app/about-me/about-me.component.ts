eded5d4d47b80664f203b834e6747a3b93df1cbb
Frank A. Kramer IV <fakramer4@gmail.com>
2026-02-19 11:54:40 -0500

    Refine the bio.

--- personal-site/src/app/about-me/about-me.component.ts
+++ personal-site/src/app/about-me/about-me.component.ts
@@ -8,7 +8,7 @@ import { NavbarComponent } from '../navbar/navbar.component';
     <app-navbar />
     <div class="container">
       <h1>Bio</h1>
-      <p>I'm a dude. Dudes, being a subset of humans, I do a lot of things. One of those things is developing software. In the breathtaking complexities of the modern global economy, I'm fortunate enough to be in a position where I can trade the time I spend developing specific software for the resources I need to sustain myself and my family. It's a wild time to be alive.</p>
+      <p>I'm a dude. Dudes, being a subset of humans, do a lot of things. As a human doing things, one of them is developing software. In the breathtaking complexities of the modern global economy, I'm fortunate enough to be in a position where I can trade the time I spend developing specific software for the resources I need to sustain myself and my family. It's a wild time to be alive.</p>
     </div>
   `,
   styleUrl: './about-me.component.scss'
